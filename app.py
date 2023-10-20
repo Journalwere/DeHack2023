1e6df707b9b10b091e29b17200c9b1dee2acce3c
Journalwere <142886974+Journalwere@users.noreply.github.com>
2023-10-20 16:16:42 -0700

    installed tools

--- app.py
+++ app.py
@@ -1,10 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template
+import keras.models
+from PIL import Image, ImageOps
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = keras.models.load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+
+        # Save file locally
+        file.save('uploaded_file.jpg')
+
+        # Load and prepare the image
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        image = Image.open('uploaded_file.jpg').convert("RGB")
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+        image_array = np.asarray(image)
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+        data[0] = normalized_image_array
+
+        # Predicts the model
+        prediction = model.predict(data)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+
+        # Delete the temporary file
+        import os
+        os.remove('uploaded_file.jpg')
+
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", confidence_score)
+
+        # Return prediction as response
+        return f"Class: {class_name[2:]}, Confidence Score: {confidence_score}"
+
+    # Display an HTML form for uploading a file
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
